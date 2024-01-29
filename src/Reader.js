297d2e38a2612590c1a165f8358337d62fe1c358
minaremote <s246276@wakayama-u.ac.jp>
2024-01-29 18:39:42 +0900

    全然うまく行ってないけど理論上はストレージから動画持ってくるコード

---
+++ src/Reader.js
@@ -0,0 +1,19 @@
+// Reader.js
+
+import { storage } from "./firebase";
+
+// Firebase Storageから動画をダウンロード
+const videoRef = storage.ref().child('videos/example.mp4'); // パスは保存した動画のパスに変更
+const video = document.getElementById('video_sample');
+
+videoRef.getDownloadURL().then((url) => {
+  video.src = url;
+  video.setAttribute('crossorigin', 'anonymous'); // CORSポリシーの制約を回避するために crossorigin を設定
+}).catch((error) => {
+  console.error('動画のダウンロードに失敗しました', error);
+});
+
+// クリック時に動画再生
+window.addEventListener('click', () => {
+  video.play();
+});
