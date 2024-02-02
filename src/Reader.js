e41deee463f832fd9005d8dfe7fe7fd97c79e85c
minaremote <s246276@wakayama-u.ac.jp>
2024-02-02 19:42:29 +0900

    マーカー取得時にメッセージとギフトボックスを出現させるように変更

--- src/Reader.js
+++ src/Reader.js
@@ -2,16 +2,13 @@ import { storage, ref, getDownloadURL } from "./firebase.js";
 
 // Firebase Storageから動画をダウンロード
 const videoRef = ref(storage, 'videos/taga.mp4'); // パスは保存した動画のパスに変更
-const thumbnailRef = ref(storage, 'img/sample3.jpg')
-
 const video = document.getElementById('video_sample');
-const thumbnail = document.getElementById('thumbnail');
-
 const arVideo = document.getElementById("arVideo")
-const thumbnailImg = document.getElementById("thumbnailImg")
-
 console.log(videoRef)
-console.log(thumbnailRef)
+
+const thumbnailText = document.getElementById("thumbnailText")
+const gift_box = document.getElementById("gift_box")
+
 
 getDownloadURL(videoRef).then((url) => {
   video.setAttribute('crossorigin', 'anonymous'); // CORSポリシーの制約を回避するために crossorigin を設定
@@ -20,16 +17,10 @@ getDownloadURL(videoRef).then((url) => {
   console.error('動画のダウンロードに失敗しました', error);
 });
 
-getDownloadURL(thumbnailRef).then((url) => {
-  thumbnail.setAttribute('crossorigin', 'anonymous'); // CORSポリシーの制約を回避するために crossorigin を設定
-  // thumbnail.src = url;
-}).catch((error) => {
-  console.error('サムネイル画像のダウンロードに失敗しました', error);
-});
-
 // クリック時に動画再生
 window.addEventListener("click", ()=>{
-  thumbnailImg.setAttribute("visible", false)
+  thumbnailText.setAttribute("visible", false)
+  gift_box.setAttribute("visible", false)
   arVideo.setAttribute("visible", true)
 
   video.play()
