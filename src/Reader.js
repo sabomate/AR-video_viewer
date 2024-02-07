d6e0f8a6a2edcd6e815e72d77e7e13c57f2a2b7a
minaremote <s246276@wakayama-u.ac.jp>
2024-02-07 16:51:49 +0900

    add:ボタンによるピン留めの指定・解除の実装

--- src/Reader.js
+++ src/Reader.js
@@ -1,6 +1,12 @@
 import { storage, ref, getDownloadURL, listAll} from "./firebase.js";
 
+// assetのビデオタグ要素
 const video = document.getElementById('arVideo');
+// ピン止め用ビデオ要素
+const pinVideo = document.getElementById("pinVideo");
+// ビデオのピンボタン
+const  VideoPinBtn = document.getElementById('VideoPinBtn');
+const videoFrame = document.getElementById("videoFrame");
 
 // Firebase Storageから動画をダウンロード
 const videoUrlArr = []
@@ -23,6 +29,7 @@ listAll(listRef)
         videoUrlArr.push(url);
         if (index == 0){
           video.src = url;
+          pinVideo.src = url;
         }
       }).catch((error) => {
         console.error(`動画のダウンロードに失敗しました: ${error}`);
@@ -31,6 +38,27 @@ listAll(listRef)
   }).catch((error) => {
     console.error(`動画のダウンロードに失敗しました: ${error}`);
   });
+
+VideoPinBtn.addEventListener('click', () => {
+  console.log("click pin btn")
+  // 切り替え時にどちらか片方が表示されるようにする
+  pinVideo.classList.toggle('hidden');
+  // videoFrame.classList.toggle('hidden');
+  videoFrame.setAttribute("visible", !videoFrame.getAttribute("visible"));
+  console.log(pinVideo.classList);
+  console.log(videoFrame.classList);
+
+  // 表示されている方を再生する
+  if (!pinVideo.classList.contains('hidden')) {
+    video.pause()
+    pinVideo.play();
+    console.log("pin video on");
+  } else {
+    pinVideo.pause();
+    videoFrame.play();
+    console.log("ar video on");
+  }
+});
 
 let videoIndex = 0;
 let videoClickCount = 0;
@@ -54,8 +82,17 @@ const  changeNextVideoBtn = document.getElementById('changeNextVideoBtn');
 changeNextVideoBtn.addEventListener('click', () => {
   videoIndex = (videoIndex + 1) % videoUrlArr.length;
   console.log('changeNextVideoBtn click' + videoIndex + ' ' + videoUrlArr[videoIndex]);
+  pinVideo.src = videoUrlArr[videoIndex];
   video.src = videoUrlArr[videoIndex];
-  video.play();
+  // 表示されている方を再生する
+  if (!pinVideo.classList.contains('hidden')) {
+    pinVideo.play();
+    console.log("pin video on");
+  } else {
+    videoFrame.play();
+    console.log("ar video on");
+  }
+  // video.play();
 });
 
 // 前の動画ボタン
@@ -63,8 +100,17 @@ const  changePreviousVideoBtn = document.getElementById('changePreviousVideoBtn'
 changePreviousVideoBtn.addEventListener('click', () => {
   videoIndex = (videoIndex - 1 + videoUrlArr.length) % videoUrlArr.length;
   console.log('changePreviousVideoBtn click' + videoIndex + ' ' + videoUrlArr[videoIndex]);
+  pinVideo.src = videoUrlArr[videoIndex];
   video.src = videoUrlArr[videoIndex];
-  video.play();
+  // 表示されている方を再生する
+  if (!pinVideo.classList.contains('hidden')) {
+    pinVideo.play();
+    console.log("pin video on");
+  } else {
+    videoFrame.play();
+    console.log("ar video on");
+  }
+  // video.play();
 });
 
 const nft = document.getElementById('nft');
@@ -72,9 +118,11 @@ const nft = document.getElementById('nft');
 nft.addEventListener('markerFound', () => {
   console.log('nft markerFound');
   video.src = videoUrlArr[videoIndex];
+  pinVideo.src = videoUrlArr[videoIndex];
   playVideBtn.classList.remove('hidden');
   changeNextVideoBtn.classList.remove('hidden');
   changePreviousVideoBtn.classList.remove('hidden');
+  VideoPinBtn.classList.toggle('hidden');
 });
 
 // marker消失時のイベント
@@ -83,4 +131,5 @@ nft.addEventListener('markerLost', () => {
   playVideBtn.classList.add('hidden');
   changeNextVideoBtn.classList.add('hidden');
   changePreviousVideoBtn.classList.add('hidden');
+  VideoPinBtn.classList.toggle('hidden');
 });
