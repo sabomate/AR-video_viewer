59e33a1a117a853dba3ec9487f20bcaedf469e5a
minari515 <126942302+minari515@users.noreply.github.com>
2024-02-21 11:32:18 +0900

    fix:IOSでのピン動画再生処理の挙動修正

--- src/Reader.js
+++ src/Reader.js
@@ -24,6 +24,7 @@ const viewStates = {
   isPinView: "pinView",
 };
 
+let deviceType;
 
 // プレゼント開封のフラグ
 // TODO: 意味が逆な気がする
@@ -60,21 +61,24 @@ function adjustPositionForPlatform() {
 
   // iOSの場合の位置調整
   if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
-      console.log("IOS")
+      console.log("IOS");
+      deviceType = "IOS";
       thumnailText.setAttribute('position', '50 -132 -50');
       giftBox.setAttribute('position', '180 -130 120');
       videoFrame.setAttribute('position', '180 55 150');
-  }
-  // Androidの場合の位置調整
+    }
+    // Androidの場合の位置調整
   else if (/Android/.test(navigator.userAgent)) {
-      console.log("android OS")
+      console.log("android OS");
+      deviceType = "android";
       thumnailText.setAttribute('position', '20 2 -400');
       giftBox.setAttribute('position', '150 100 -200');
       videoFrame.setAttribute('position', '150 155 -300');
   }
   // それ以外（PCなど）の場合の位置調整
   else {
-      console.log("else OS")
+      console.log("else OS");
+      deviceType = "PC";
       thumnailText.setAttribute('position', '150 2 -250');
       giftBox.setAttribute('position', '300 100 -100');
       videoFrame.setAttribute('position', '300 105 -100');
@@ -121,7 +125,7 @@ await listAll(listRef)
 if (personal != null) {
   const personalListRef = ref(storage, "videos/personals/" + personal);
   await listAll(personalListRef)
-    .then(async (res) => {    
+    .then(async (res) => {
       const prefixes = res.prefixes;
       for(const prefix of prefixes) {
         let thumbnailRef, videoRef,thumbnailUrl;
@@ -235,16 +239,19 @@ function changeVideo(videoIndex) {
 // ピン状態の再生処理
 playPinVideBtn.addEventListener("click", () => {
   pinVideoFrame.play();
-  playPinVideBtn.classList.add("hidden");
+  if(!deviceType==="IOS"){
+    playPinVideBtn.classList.add("hidden");
+  }
 });
 
 // ピン止め動画変更時のボタン表示
-pinVideoFrame.addEventListener("canplay", () => {
-  console.log("video can play")
+pinVideoFrame.addEventListener("loadedmetadata", () => {
+  console.log("video metadata loaded");
   if (currentViewState === viewStates.isPinView && pinVideoFrame.paused) {
     playPinVideBtn.classList.remove("hidden");
   }
 });
+
 
 // ピン止め動画の再再生
 pinVideoFrame.addEventListener("ended", function () {
